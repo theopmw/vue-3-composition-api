2ecc538c4c6368462840804c42d339a01bfd94cd
theopmw <theowrightdev@gmail.com>
2023-09-05 18:18:36 +0100

    Add state properties to pinia store

--- vue-composition-api-basics/src/stores/counter.js
+++ vue-composition-api-basics/src/stores/counter.js
@@ -1,16 +1,9 @@
-import { defineStore } from 'pinia'
+import { defineStore } from 'pinia';
 
 export const useCounterStore = defineStore({
   id: 'counter',
   state: () => ({
-    counter: 0
+    count: 0,
+    title: 'My Counter Title',
   }),
-  getters: {
-    doubleCount: (state) => state.counter * 2
-  },
-  actions: {
-    increment() {
-      this.counter++
-    }
-  }
-})
+});
